41b59f4e594075cebc344a537f77c9698e1d6015
SoniGama <abinyuuu@gmail.com>
2018-04-24 07:17:00 -0500

    js

---
+++ js/app.js
@@ -0,0 +1,22 @@
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src =
+        'https://connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v2.12&appId=613785305632228&autoLogAppEvents=1';
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src =
+        'https://connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v2.12&appId=213795999353853&autoLogAppEvents=1';
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
+// 1007162108587-4m51flnbq35262muv120p75luni5ihls.apps.googleusercontent.com
